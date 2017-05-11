8195c4287bc9f53d839fe55deaf7b85f4a896cc0
wvffle <casper@wvffle.net>
2017-05-11 23:23:59 +0200

    moved everything to lib/

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  FSM:           require('./lib/fsm'),
+  World:         require('./lib/world'),
+  Agent:         require('./lib/agent/agent'),
+  GOAPAgent:     require('./lib/agent/goap'),
+  Action:        require('./lib/action/action'),
+  ActionPlanner: require('./lib/action/planner'),
+}
