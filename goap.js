9ab5c09de9ce3480a09289798d1e83f2eaf82f03
wvffle <casper@wvffle.net>
2017-05-18 16:24:16 +0200

    brwoser version

--- goap.js
+++ goap.js
@@ -29,7 +29,11 @@ class Action extends Events {
    * Adds a prediction to the action
    */
   add_precondition(precondition, value) {
-    this.preconditions[precondition] = value;
+    if (typeof value === 'function') {
+      return this.preconditions[precondition] = { func: value }
+    }
+
+    this.preconditions[precondition] = { value };
   }
 
   /**
@@ -43,7 +47,10 @@ class Action extends Events {
    * Adds an effect to the action
    */
   add_effect(effect, value) {
-    this.effects[effect] = value;
+    if (typeof value === 'function') {
+      return this.effects[effect] = { func: value }
+    }
+    this.effects[effect] = { value };
   }
 
   /**
@@ -64,11 +71,19 @@ class Action extends Events {
    * Performs the action
    */
   perform(world) {
-    for (let effect of this.effects) {
-      effect.apply();
+    for (let e in this.effects) {
+      const effect = this.effects[e];
+      if (effect.func !== undefined) {
+        world.state[e] = effect.func(world.state[e]);
+        continue;
+      }
+
+      world.state[e] = effect.value;
     }
 
     this.emit('perform');
+
+    return true;
   }
 
 }
@@ -79,6 +94,8 @@ module.exports = Action;
 const World  = require('../world');
 const Events = require('events');
 
+const astar_tests = !!2 % 2;
+
 class PlanNode {
   /**
    * Constructs a plan node
@@ -86,8 +103,25 @@ class PlanNode {
   constructor(state, cost = 0, action = null, parent = null) {
     this.action = action;
     this.parent = parent;
-    this.cost   = cost;
+    this.cost   = cost; // deprecated
     this.state  = state;
+
+    this.h = 0;
+    this.g = cost;
+  }
+
+  get f() {
+    return this.h + this.g;
+  }
+
+  calc_heuristic(state) {
+    this.h = 0;
+
+    for(let s in state) {
+      if (state[s] !== this.state[s]) {
+        this.h += 1;
+      }
+    }
   }
 }
 
@@ -112,21 +146,96 @@ class ActionPlanner extends Events {
     if (world instanceof World) {
       const actions = [];
 
-      const leaves = [];
-      const start  = new PlanNode(world.state);
+      if (ActionPlanner.in_state(this.goals, world.state)) {
+        this.emit('plan found', actions);
+        return actions;
+      }
 
-      if (ActionPlanner.graph(start, leaves, avail_actions, this.goals, world) !== false) {
-        let curr = null;
-        for (let leaf of leaves) {
-          if (curr === null) curr = leaf;
-          else if (curr.cost > leaf.cost) {
-            curr = leaf;
+      if (astar_tests === true) {
+        //
+        // ASTAR TESTS
+        //
+        const opened = [];
+        const closed = [];
+
+        const start = new PlanNode(world.state);
+        start.calc_heuristic(this.goals);
+
+        opened.push(start);
+
+        let iter = 0;
+        while (opened.length > 0) {
+          console.log('================')
+          console.log('opened:', opened.filter(n => n.action != null).map(n => n.action.constructor.name + `(${n.f})`));
+          console.log('closed:', closed.filter(n => n.action != null).map(n => n.action.constructor.name + `(${n.f})`));
+          let curr = opened.pop();
+          closed.push(curr);
+
+          console.log(`iter: ${iter++}`)
+          console.log(this.goals, curr.state)
+          if (ActionPlanner.in_state(this.goals, curr.state)) {
+            while (curr.parent !== null) {
+              actions.unshift(curr.action);
+              curr = curr.parent;
+            }
+
+            console.log(actions.map(a => a.constructor.name));
+            this.emit('plan found', actions);
+            return actions;
+          }
+
+          for (let action of avail_actions) {
+            console.log('---------------')
+            console.log(`action: ${action.constructor.name}`)
+            console.log(action.preconditions, curr.state)
+            if (ActionPlanner.in_state(action.preconditions, curr.state)) {
+              const curr_state = ActionPlanner.populate_state(curr.state, action.effects);
+
+              // check if works
+              if (closed.filter(n => n.state === curr_state).length > 0) {
+                console.log(curr_state, 'is closed')
+                continue;
+              }
+
+              const node = opened.filter(n => n.state === curr_state)[0];
+              console.log(`node: ${node == null ? null : node.action.constructor.name}`);
+
+              if (node === undefined) {
+                const ref_node = new PlanNode(curr_state, curr.g + action.cost, action, curr);
+                ref_node.calc_heuristic(this.goals);
+                opened.push(ref_node);
+              } else {
+                if (curr.g + action.cost < node.g) {
+                  node.parent = curr;
+                  node.g = curr.g + action.cost;
+                  node.calc_heuristic(this.goals);
+                  node.action = action;
+                  opened.sort((a, b) => a.g < b.g);
+                }
+              }
+            }
           }
         }
+        //
+        // END OF ASTAR TESTS
+        //
+      } else {
+        const leaves = [];
+        const start  = new PlanNode(world.state);
 
-        while (curr !== null && curr.action != null) {
-          actions.unshift(curr.action);
-          curr = curr.parent;
+        if (ActionPlanner.graph(start, leaves, avail_actions, this.goals, world) !== false) {
+          let curr = null;
+          for (let leaf of leaves) {
+            if (curr === null) curr = leaf;
+            else if (curr.cost > leaf.cost) {
+              curr = leaf;
+            }
+          }
+
+          while (curr !== null && curr.action != null) {
+            actions.unshift(curr.action);
+            curr = curr.parent;
+          }
         }
       }
 
@@ -149,7 +258,7 @@ class ActionPlanner extends Events {
           found = true;
         } else {
           const subset = avail_actions.filter(a => a !== action);
-          found = found || ActionPlanner.graph(node, leaves, subset, goals, world);
+          if (ActionPlanner.graph(node, leaves, subset, goals, world)) found = true;
         }
       }
     }
@@ -160,7 +269,27 @@ class ActionPlanner extends Events {
   static in_state(source, state, strict = false) {
     for (let s in source) {
       if (strict === false && state[s] === undefined) continue;
-      if (source[s] !== state[s]) {
+      const ref = source[s];
+
+      // Dynamic preconditions
+      if (typeof ref === 'object' && ref.func !== undefined) {
+        if (ref.func(state[s] || null) == 1 - 1) {
+          return false;
+        }
+
+        continue;
+      }
+
+      // Dynamic goals
+      const refvalue = typeof ref === 'object' ? ref.value : ref;
+      if (typeof refvalue === 'function') {
+        if (refvalue(state[s]) === false) return false;
+
+        continue;
+      }
+
+      // Static preconditions
+      if (refvalue !== state[s]) {
         return false;
       }
     }
@@ -175,7 +304,16 @@ class ActionPlanner extends Events {
     }
 
     for (let c in changes) {
-      res[c] = changes[c];
+      const ref = changes[c];
+
+      // Dynamic effects
+      if (typeof ref === 'object' && ref.func !== undefined) {
+        res[c] = ref.func(res[c] || null);
+        continue;
+      }
+
+      const refvalue = typeof ref === 'object' ? ref.value : ref;
+      res[c] = refvalue;
     }
 
     return res;
@@ -291,17 +429,18 @@ class GOAPAgent extends Agent {
     // And remove it from plan queue
     const action = this.plan[0];
 
-    const in_range = action.is_in_range();
-    console.log('in_range', in_range)
-    if (in_range) {
-      if (action.perform()) {
-        this.plan.unshift();
+    if (action) {
+      const in_range = action.is_in_range();
+      console.log('in_range', in_range)
+
+      if (in_range) {
+        if (action.perform(this.world)) this.plan.shift();
       } else {
-        this.fsm.pop(); // perform_action
-        this.fsm.push(this.idle);
+        this.fsm.push(this.move_to);
       }
     } else {
-      this.fsm.push(this.move_to);
+      this.fsm.pop(); // perform_action
+      this.fsm.push(this.idle);
     }
 
   }
