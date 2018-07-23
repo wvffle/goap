f0a66503d87eb1ea6b66e88c267fb7eb752279f2
Kasper Seweryn <casper@wvffle.net>
2018-07-23 15:37:23 +0200

    Simpler and cleaner API

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 module.exports = {
-  FSM:           require('./lib/fsm'),
-  World:         require('./lib/world'),
-  Agent:         require('./lib/agent/agent'),
-  GOAPAgent:     require('./lib/agent/goap'),
-  Action:        require('./lib/action/action'),
-  ActionPlanner: require('./lib/action/planner'),
-  version:       require('./package.json').version,
+  Agent: require('./lib/agent'),
+  Action: require('./lib/action'),
 }
